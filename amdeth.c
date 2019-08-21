89f701ac57502c35aaef7646fb318a32edc8c3c4
till straumann <till.straumann@alumni.tu-berlin.de>
2019-08-21 15:20:37 -0700

    amdeth.c: use 'wrap.h' from source directory (no extra module generated)

--- amdeth.c
+++ amdeth.c
@@ -25,7 +25,7 @@
 #include <netinet/in.h>	/* htons */
 
 #include "amdeth.h"
-#include "wrap/wrap.h"
+#include "wrap.h"
 
 #define NUM_ETH_DEVICES 4	/* how many devices our table supports */
 #define RT_DRIVER			/* whether to set some flags for real-time specific application */
